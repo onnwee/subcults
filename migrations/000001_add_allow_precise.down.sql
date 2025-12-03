d789b2733f13566dd33a08a374513e39375cde7d
⓪ηηωεε忧世 <onnweexd@gmail.com>
2025-12-03 10:15:23 -0600

    Revert "Add golang-migrate script with up/down support and documentation"

--- migrations/000001_add_allow_precise.down.sql
+++
@@ -1,7 +0,0 @@
--- Rollback: Remove allow_precise column from scenes and events tables
-
--- Remove allow_precise column from events table
-ALTER TABLE events DROP COLUMN IF EXISTS allow_precise;
-
--- Remove allow_precise column from scenes table
-ALTER TABLE scenes DROP COLUMN IF EXISTS allow_precise;
